245ad707c0ec4a286d46848661927445b5f878e8
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-08-03 23:08:41 +0200

    Implement basic noise/height map generation and visualization

---
+++ Assets/MapGen.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+    public uint size = 500;
+    public float scale = 110.3f;
+    public float offsetX = 0;
+    public float offsetY = 0;
+
+    public Material noisePlaneMaterial;
+
+    private MeshFilter meshFilter;
+    private GameObject noisePlane;
+    private bool update = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (noisePlane == null)
+            noisePlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+        GenerateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (update)
+        {
+            update = false;
+            GenerateMap();
+        }
+    }
+
+    void OnValidate()
+    {
+        if (size < 10) size = 10;
+        if (scale < 0.1f) scale = 0.1f;
+        if (offsetX < 0) offsetX = 0;
+        if (offsetY < 0) offsetY = 0;
+        update = true;
+    }
+
+    void GenerateMap()
+    {
+        float[,] heightMap = generatePerlinNoiseMap(size, offsetX, offsetY, scale);
+        GenerateNoiseTexture(heightMap);
+    }
+
+    void GenerateNoiseTexture(float[,] noiseMap)
+    {
+        int mapWidth = noiseMap.GetLength(0);
+        int mapHeight = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(mapWidth, mapHeight);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                colourMap[y * mapWidth + x] =
+                    Color.Lerp(Color.black, Color.white, noiseMap[mapWidth - x - 1, mapHeight - y - 1]);
+            }
+        }
+        texture.SetPixels(colourMap);
+        texture.Apply();
+
+        noisePlane.GetComponent<Renderer>().material = noisePlaneMaterial;
+        noisePlane.GetComponent<Renderer>().material.mainTexture = texture;
+        noisePlane.transform.localScale = new Vector3(mapWidth / 10.0f, 1, mapHeight / 10.0f);
+        noisePlane.transform.position = gameObject.transform.position;
+    }
+
+    public static float[,] generatePerlinNoiseMap(uint size, float offsetX, float offsetY, float scale)
+    {
+        float[,] heightMap = new float[size, size];
+        for (float y = 0; y < size; y++)
+        {
+            for (float x = 0; x < size; x++)
+            {
+                float pnX = (offsetX + x) / scale;
+                float pnY = (offsetY + y) / scale;
+                heightMap[(int)x, (int)y] = Mathf.PerlinNoise(pnX, pnY);
+            }
+        }
+
+        return heightMap;
+    }
+
+}
