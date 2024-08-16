524e15dd997628830482eb2ceba284d7f54d3fc1
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-08-16 19:19:38 +0200

    Refactor, move structure definition from `MapGen` to `GenerationConfig`

---
+++ Assets/Scripts/GenerationConfig.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct NoiseMapConfig
+{
+    public uint size;
+    public float scale;
+    public bool offsetBySize;
+    public float offsetX;
+    public float offsetY;
+
+    public NoiseMapConfig(uint size, float scale, bool offsetBySize, float offsetX, float offsetY)
+    {
+        this.size = size;
+        this.scale = scale;
+        this.offsetBySize = offsetBySize;
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+
+    }
+}
+
+[Serializable]
+public struct FBmParams
+{
+    public float amplitude;
+    public float frequency;
+    public float persistence;
+    public float lacunarity;
+    public uint octaveCount;
+
+    public FBmParams(
+            float amplitude,
+            float frequency,
+            float persistence,
+            float lacunarity,
+            uint octaveCount)
+    {
+        this.amplitude = amplitude;
+        this.frequency = frequency;
+        this.persistence = persistence;
+        this.lacunarity = lacunarity;
+        this.octaveCount = octaveCount;
+    }
+}
+
+[Serializable]
+public struct ColorThreshold
+{
+    public Color color;
+    public float threshold;
+
+    public ColorThreshold(Color color, float threshold)
+    {
+        this.color = color;
+        this.threshold = threshold;
+    }
+}
